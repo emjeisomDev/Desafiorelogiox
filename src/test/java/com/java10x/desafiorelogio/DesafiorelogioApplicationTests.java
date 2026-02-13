bde36731787762d7df6da04a36410ab8bd6b8ba0
Emanuel Jeisom <emjeisom.dev@gmail.com>
2026-02-13 16:49:47 -0300

    Project Created

---
+++ src/test/java/com/java10x/desafiorelogio/DesafiorelogioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java10x.desafiorelogio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiorelogioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
